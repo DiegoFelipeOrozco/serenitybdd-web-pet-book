addf15edb0c97a61c3dbae8f06d28aebf94be791
Diego Felipe Orozco Penagos <Dioro1999@gmail.com>
2020-10-03 18:27:17 -0500

    Mejoras en la logica del escenario y salida del reporte When con adaptable memorizacion de atributos de elemento a dar click

--- src/test/java/co/com/petBook/stepdefinitions/PetBookStepDefinition.java
+++ src/test/java/co/com/petBook/stepdefinitions/PetBookStepDefinition.java
@@ -7,6 +7,8 @@ import cucumber.api.java.es.Entonces;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.RememberThat;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.questions.Attribute;
+import net.serenitybdd.screenplay.questions.CSSValue;
 import net.serenitybdd.screenplay.actions.Open;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
@@ -21,23 +23,26 @@ public class PetBookStepDefinition {
 
         theActor.attemptsTo(
                 Open.url(PetBookPage.HOME),
-                RememberThat.theValueOf("imagen seleccionada").is(
-                        "assets/images/perro1.jpg"),
-                RememberThat.theValueOf("alto de la imagen").is(
-                        "170px"),
-                RememberThat.theValueOf("ancho de la imagen").is(
-                        "240px"),
+                RememberThat.theValueOf("imagen seleccionada").isAnsweredBy(
+                        Attribute.of(PetBookPage.FIRST_IMAGE).named("src").viewedBy(theActor).asAQuestion()
+                ),
+                RememberThat.theValueOf("alto de la imagen").isAnsweredBy(
+                        CSSValue.of(PetBookPage.FIRST_IMAGE).named("height").viewedBy(theActor).asAQuestion()
+                ),
+                RememberThat.theValueOf("ancho de la imagen").isAnsweredBy(
+                        CSSValue.of(PetBookPage.FIRST_IMAGE).named("width").viewedBy(theActor).asAQuestion()
+                ),
                 Click.on(PetBookPage.FIRST_IMAGE)
         );
     }
 
     @Entonces("^debe ver dicha imagen de mayor tamaño$")
-    public void debeVerDichaImagenDelTamañoDeLaPantalla() {
+    public void deberiaVerLaImagenMasGrande() {
         theActorInTheSpotlight().should(seeThat(
                 TheFocusedImage.isSourcedFrom(theActorInTheSpotlight().<String>recall("imagen seleccionada")).
                         andIsBiggerThan(
                                 theActorInTheSpotlight().<String>recall("alto de la imagen"),
                                 theActorInTheSpotlight().<String>recall("ancho de la imagen"))
-        ));
+        ).because("la imagen enfocada es la seleccionada y es mas grande"));
     }
 }
